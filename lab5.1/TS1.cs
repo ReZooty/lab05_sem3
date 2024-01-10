28b2132f66247626fac2a8885c0c10fb52754b7d
ReZooty <62943549+ReZooty@users.noreply.github.com>
2024-01-11 00:03:51 +0300

    Add files via upload

---
+++ lab5.1/TS1.cs
@@ -0,0 +1,107 @@
+﻿namespace TS1
+{
+    abstract class Program
+    {
+        class MyMatrix
+        {
+            private List<List<double>> _matrix;
+
+            private int N => _matrix.Count;
+
+            private int M => _matrix[0].Count;
+
+            public MyMatrix(int n, int m, int min, int max)
+            {
+                Random rand = new Random();
+                _matrix = new List<List<double>>();
+                for (int j = 0; j < n; j++)
+                {
+                    _matrix.Add(new List<double>());
+                    for (int i = 0; i < m; i++)
+                    {
+                        _matrix[j].Add(rand.Next(min, max));
+                    }
+                }
+            }
+
+            public void Fill(int min, int max)
+            {
+                Random rand = new Random();
+                for (int j = 0; j < N; j++)
+                {
+                    for (int i = 0; i < M; i++)
+                    {
+                        _matrix[j][i] = rand.Next(min, max);
+                    }
+                }
+            }
+
+            public void ChangeSize(int nNew, int mNew, int min, int max)
+            {
+                Random rand = new Random();
+                List<List<double>> newMatrix = new List<List<double>>(nNew);
+                for (int i = 0; i < nNew; i++)
+                {
+                    for (int j = 0; j < mNew; j++)
+                    {
+                        newMatrix.Add(new List<double>());
+                        if (i < N && j < M)
+                        {
+                            newMatrix[i].Add(_matrix[i][j]);
+                        }
+                        else
+                        {
+                            newMatrix[i].Add(rand.Next(min, max));
+                        }
+                    }
+                }
+
+                _matrix = newMatrix;
+            }
+
+            public void ShowPartialy(int nStart, int nFinish, int mStart, int mFinish)
+            {
+                for (int i = nStart - 1; i < nFinish; i++)
+                {
+                    for (int j = mStart - 1; j < mFinish; j++)
+                    {
+                        Console.Write($"{_matrix[i][j]} ");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+
+            public void Show()
+            {
+                foreach (var row in _matrix)
+                {
+                    foreach (var num in row)
+                    {
+                        Console.Write($"{num} ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            public double this[int a, int b]
+            {
+                get => this._matrix[a][b];
+                set => this._matrix[a][b] = value;
+            }
+        }
+
+        static void Main()
+        {
+            MyMatrix matrix = new MyMatrix(4, 4, 1, 10);
+            matrix.Show();
+            Console.WriteLine();
+            matrix.Fill(1, 10);
+            matrix.ShowPartialy(1, 2, 1, 2);
+            Console.WriteLine();
+            matrix.ChangeSize(5, 5, 1, 10);
+            matrix.Show();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ lab5.2/TS2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+class MyList<T> : IEnumerable<T>
+{
+    private T[] list;
+    int length
+    {
+        get => list.Length;
+    }
+    public MyList(params T[] args)
+    {
+        int c = 0;
+        list = new T[args.Length];
+        foreach (T i in args)
+        {
+            list[c] = i;
+            c++;
+        }
+    }
+    public void Add(T new_element)
+    {
+        var new_list = new T[this.list.Length + 1];
+        list.CopyTo(new_list, 0);
+        new_list[list.Length] = new_element;
+        list = new_list;
+    }
+    public T this[int i]
+    {
+        get => list[i];
+        set => list[i] = value;
+    }
+    public void Print()
+    {
+        foreach (T i in list)
+        {
+            Console.Write(i + "\t");
+        }
+        Console.WriteLine();
+    }
+    public IEnumerator<T> GetEnumerator()
+    {
+        return (IEnumerator<T>)list.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
